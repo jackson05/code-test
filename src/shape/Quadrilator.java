be51d79a58041e664ad4ef3ff3dc1ca439fefe35
Jackson RUGAMBA <68805294+jackson05@users.noreply.github.com>
2024-01-29 13:03:16 +0100

    Major change in building and printing the out put

--- src/shape/Quadrilator.java
+++ src/shape/Quadrilator.java
@@ -1,9 +1,35 @@
 package shape;
 public class Quadrilator{
 
-    public void square(n){
-        int row=0;
-        int col=0;
-        
+    public Quadrilator(){
+
+    }
+
+    public void buildRow(int col){
+        for(int i=1;i<=col;i++){
+                System.out.print("*")
+        }
+    }
+
+    public void builRowWhiteSpace(int colSize){
+        for(int i=1; i <= colSize;i++){
+            if(i==1 || i== colSize){
+                System.out.print("*")
+            }else{
+                System.out.print(" ")
+            }
+        }
+    }
+
+    public void buildSquare(int n){
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n; col++){
+                if(row==1 || row ==n){
+                    buildRow(n);
+                }else{
+                    builRowWhiteSpace(n);
+                }
+            }
+        }
     }
 }
