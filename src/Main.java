8343dfdefcdde3bc361a418f4582eb6d4b80f0e0
jackson05 <jackson5g.bi@gmail.com>
2024-01-29 23:22:51 +0100

    algorithm optimized and bug fixed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
    
     public static void main(String[] args) {
     
-        Quadrilator q=new Quadrilator()
+        Quadrilator q=new Quadrilator();
         q.buildSquare(4);
     }
 }
--- src/shape/Quadrilator.java
+++ src/shape/Quadrilator.java
@@ -1,35 +1,21 @@
 package shape;
 public class Quadrilator{
 
-    public Quadrilator(){
-
-    }
-
-    public void buildRow(int col){
-        for(int i=1;i<=col;i++){
-                System.out.print("*")
-        }
-    }
-
-    public void builRowWhiteSpace(int colSize){
-        for(int i=1; i <= colSize;i++){
-            if(i==1 || i== colSize){
-                System.out.print("*")
-            }else{
-                System.out.print(" ")
-            }
-        }
-    }
-
-    public void buildSquare(int n){
-        for(int row=1; row<=n; row++){
+    public void buildSquare(int n,int m){
+        for(int row=1; row<=m; row++){
             for(int col=1; col<=n; col++){
-                if(row==1 || row ==n){
-                    buildRow(n);
-                }else{
-                    builRowWhiteSpace(n);
+                if(row==1 || row ==m){
+                    System.out.print("*");
+                }
+                else{
+                    if(col==1 || col== n){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
                 }
             }
+            System.out.println();
         }
     }
 }
